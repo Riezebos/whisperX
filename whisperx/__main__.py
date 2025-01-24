82c97e9af78c5894b78f24b974d4a00cb96c5dca
Riezebos <22647971+Riezebos@users.noreply.github.com>
2025-01-24 15:53:35 +0100

    Try autoload cudnn

--- whisperx/__main__.py
+++ whisperx/__main__.py
@@ -1,4 +1,10 @@
+from pathlib import Path
+import os
+
 from .transcribe import cli
 
+cudnn_path = Path(__file__).parents[1] / "/nvidia/cudnn/lib/"
+
+os.environ["LD_LIBRARY_PATH"] = cudnn_path
 
 cli()
